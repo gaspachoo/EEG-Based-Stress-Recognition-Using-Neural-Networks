162bb2cc136c674a47831d056f4bdfc22ff641df
Gaspachoo <gaspard.lefort@outlook.fr>
2025-03-13 11:43:05 +0530

    edit data_analysis.ipynb to correct a mistake while plotting band power across tasks

--- support_func/filter_all_data.py
+++ support_func/filter_all_data.py
@@ -36,5 +36,5 @@ def filter_all_data(method_name, input_dir):
 
 if __name__ == "__main__":
     input_dir = '../Data/raw_data'
-    method_name = 'SKLFast_ICA'
+    method_name = 'bandpass_filter'
     filter_all_data(method_name,input_dir)
